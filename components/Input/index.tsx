e93da661548daf612228d16a539a2b0fb4e9427b
Jeroen Derks <zu20xy@insim.biz>
2023-05-09 15:39:23 +0200

    feat: test with export from utils

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -5,14 +5,9 @@ import { Container, Button, Form } from "./styled";
 import { blendShapeToAnimation } from "../../utils/blendShapeToAnimation";
 import type { FormProps } from "./types";
 import { BlendData } from "../../utils/types";
+import { mixer } from "../../utils/threeConfig";
 
-export const Input = ({
-  mixer,
-  updateAnimation,
-}: {
-  mixer: any;
-  updateAnimation: any;
-}) => {
+export const Input = () => {
   const handleSubmit = async (e: React.FormEvent<FormProps>) => {
     e.preventDefault();
 
@@ -25,13 +20,20 @@ export const Input = ({
       const blendData: BlendData = req.data.blendData;
       console.log(req.data);
       const clip = blendShapeToAnimation(blendData);
-      updateAnimation(clip);
 
+      console.log("clip", clip);
       const audio = new Audio(
         req.data.rawData.replace("data:;", "data:audio/mpeg;")
       );
       audio.load();
       audio.play();
+
+      let animation = mixer.clipAction(clip);
+      animation.setLoop(THREE.LoopOnce);
+      animation.clampWhenFinished = true;
+      animation.enable = true;
+
+      animation.reset().play();
     }
   };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,141 +1,20 @@
 "use client";
 
 import React, { useRef, useEffect } from "react";
-import * as THREE from "three";
-// @ts-ignore
-import Stats from "three/addons/libs/stats.module.js";
-// @ts-ignore
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-// @ts-ignore
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-// @ts-ignore
-import { KTX2Loader } from "three/addons/loaders/KTX2Loader.js";
-// @ts-ignore
-import { MeshoptDecoder } from "three/addons/libs/meshopt_decoder.module.js";
-// @ts-ignore
-import { RoomEnvironment } from "three/addons/environments/RoomEnvironment.js";
-// @ts-ignore
-import { GUI } from "three/addons/libs/lil-gui.module.min.js";
-
 import { Input } from "../../components/Input";
-
-let mixer: any;
+import { initThree } from "../../utils/threeConfig";
 
 export default function Home() {
   const ref = useRef(null);
-  const [animation, setAnimation] = React.useState<any>();
 
   useEffect(() => {
-    const clock = new THREE.Clock();
-
-    const zeroTrack = new THREE.NumberKeyframeTrack(
-      "mesh_2.morphTargetInfluences",
-      [0, 1],
-      Array.from({ length: 104 }, () => 0)
-    );
-    let zeroClip = new THREE.AnimationClip("blink", -1, [zeroTrack]);
-
-    const container = document.createElement("div");
-    document.body.appendChild(container);
-
-    const camera = new THREE.PerspectiveCamera(
-      45,
-      window.innerWidth / window.innerHeight,
-      1,
-      20
-    );
-    camera.position.set(-1.8, 0.8, 3);
-
-    const scene = new THREE.Scene();
-
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    renderer.toneMapping = THREE.ACESFilmicToneMapping;
-
-    container.appendChild(renderer.domElement);
-
-    const ktx2Loader = new KTX2Loader()
-      .setTranscoderPath("/textures/basis/")
-      .detectSupport(renderer);
-
-    new GLTFLoader()
-      .setKTX2Loader(ktx2Loader)
-      .setMeshoptDecoder(MeshoptDecoder)
-      .load("/facecap.glb", (gltf: any) => {
-        console.log("gltf", gltf);
-
-        const mesh = gltf.scene.children[0];
-        scene.add(mesh);
-        console.log("mesh", mesh);
-
-        mixer = new THREE.AnimationMixer(mesh);
-        mixer.clipAction(zeroClip).play();
-        // GUI
-
-        const head = mesh.getObjectByName("mesh_2");
-        const influences = head.morphTargetInfluences;
-
-        const gui = new GUI();
-        gui.close();
-
-        for (const [key, value] of Object.entries(head.morphTargetDictionary)) {
-          console.log(key, value);
-          gui
-            .add(influences, value, 0, 1, 0.01)
-            .name(key.replace("blendShape1.", ""))
-            .listen(influences);
-        }
-      });
-
-    const environment = new RoomEnvironment();
-    const pmremGenerator = new THREE.PMREMGenerator(renderer);
-
-    scene.background = new THREE.Color(0x666666);
-    scene.environment = pmremGenerator.fromScene(environment).texture;
-
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true;
-    controls.minDistance = 2.5;
-    controls.maxDistance = 5;
-    controls.minAzimuthAngle = -Math.PI / 2;
-    controls.maxAzimuthAngle = Math.PI / 2;
-    controls.maxPolarAngle = Math.PI / 1.8;
-    controls.target.set(0, 0.15, -0.2);
-
-    const stats = new Stats();
-    container.appendChild(stats.dom);
-
-    renderer.setAnimationLoop(() => {
-      const delta = clock.getDelta();
-      if (mixer) {
-        mixer.update(delta);
-      }
-
-      renderer.render(scene, camera);
-
-      controls.update();
-
-      stats.update();
-    });
-  }, [animation]);
-
-  const updateAnimation = (_animation: any) => {
-    if (animation) {
-      let clipAction = mixer.clipAction(_animation);
-      clipAction.setLoop(THREE.LoopOnce);
-      clipAction.clampWhenFinished = true;
-      clipAction.enable = true;
-
-      clipAction.reset().play();
-    }
-  };
+    initThree();
+  }, []);
 
   return (
     <main>
       <div ref={ref}></div>
-      <Input mixer={mixer} updateAnimation={updateAnimation} />
+      <Input />
     </main>
   );
 }
