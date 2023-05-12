4ffed726a967085c5e0378865906ff12c8a14563
JeroenDerks <jeroenderks88@gmail.com>
2023-05-12 18:55:04 +0200

    feat: add new model

--- components/VoiceInput/index.tsx
+++ components/VoiceInput/index.tsx
@@ -10,66 +10,61 @@ export const VoiceInput = () => {
   );
   const [micEnabled, setMicEnabled] = React.useState(false);
 
-  let speechRecognizer: any;
   const key = process.env.NEXT_PUBLIC_AZURE_KEY;
   const region = process.env.NEXT_PUBLIC_AZURE_REGION;
 
   if (!key || !region) {
     setText("Oops, there was an error from our side");
-  }
+  } else {
+    const speechConfig = sdk.SpeechConfig.fromSubscription(key, region);
+    speechConfig.speechRecognitionLanguage = "en-US";
 
-  const speechConfig = sdk.SpeechConfig.fromSubscription(key!, region!);
-  speechConfig.speechRecognitionLanguage = "en-US";
-
-  const audioConfig = sdk.AudioConfig.fromDefaultMicrophoneInput();
-  speechRecognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
-  speechRecognizer.recognizing = (s: any, e: any) => {
-    console.log(`RECOGNIZING: Text=${e.result.text}`);
-  };
-
-  speechRecognizer.recognized = (s: any, e: any) => {
-    if (e.result.reason == sdk.ResultReason.RecognizedSpeech) {
-      console.log(`RECOGNIZED: Text=${e.result.text}`);
+    const audioConfig = sdk.AudioConfig.fromDefaultMicrophoneInput();
+    const speechRecognizer = new sdk.SpeechRecognizer(
+      speechConfig,
+      audioConfig
+    );
+    speechRecognizer.recognizing = (s, e) => {
+      console.log(`RECOGNIZING: Text=${e.result.text}`);
       setText(e.result.text);
-    } else if (e.result.reason == sdk.ResultReason.NoMatch) {
-      console.log("NOMATCH: Speech could not be recognized.");
-      setText("Sorry, we did not recognize your text");
-    }
-  };
+    };
 
-  speechRecognizer.canceled = (s: any, e: any) => {
-    console.log(`CANCELED: Reason=${e.reason}`);
+    speechRecognizer.recognized = (s, e) => {
+      if (e.result.reason == sdk.ResultReason.RecognizedSpeech) {
+        console.log(`RECOGNIZED: Text=${e.result.text}`);
+        setText(e.result.text);
+      } else if (e.result.reason == sdk.ResultReason.NoMatch) {
+        console.log("NOMATCH: Speech could not be recognized.");
+        setText("Sorry, we did not recognize your text");
+      }
+    };
 
-    if (e.reason == sdk.CancellationReason.Error) {
-      console.log(`"CANCELED: ErrorCode=${e.errorCode}`);
-      console.log(`"CANCELED: ErrorDetails=${e.errorDetails}`);
-      console.log(
-        "CANCELED: Did you set the speech resource key and region values?"
-      );
-      setText("Please talk into the mic");
-    }
-    speechRecognizer.stopContinuousRecognitionAsync();
-  };
+    speechRecognizer.canceled = (s, e) => {
+      console.log(`CANCELED: Reason=${e.reason}`);
 
-  speechRecognizer.sessionStopped = (s: any, e: any) => {
-    console.log("\n    Session stopped event.");
-    speechRecognizer.stopContinuousRecognitionAsync();
-  };
-
-  const handleMicToggle = () => {
-    if (micEnabled) {
+      if (e.reason == sdk.CancellationReason.Error) {
+        console.log(`"CANCELED: ErrorCode=${e.errorCode}`);
+        console.log(`"CANCELED: ErrorDetails=${e.errorDetails}`);
+        console.log(
+          "CANCELED: Did you set the speech resource key and region values?"
+        );
+        setText("Please talk into the mic");
+      }
       speechRecognizer.stopContinuousRecognitionAsync();
-    } else {
-      speechRecognizer.startContinuousRecognitionAsync();
-    }
-  };
+    };
+
+    speechRecognizer.sessionStopped = (s, e) => {
+      console.log("\n    Session stopped event.");
+      speechRecognizer.stopContinuousRecognitionAsync();
+    };
+  }
 
   return (
     <Wrapper>
       <Container>
         <Copy>{text}</Copy>
       </Container>
-      <Button onClick={handleMicToggle}>
+      <Button onClick={() => setMicEnabled(true)}>
         {micEnabled ? "Mic is on" : "Enable mic"}
       </Button>
     </Wrapper>
--- utils/threeConfig.ts
+++ utils/threeConfig.ts
@@ -1,18 +1,15 @@
 import * as THREE from "three";
 // @ts-ignore
-// import Stats from "three/addons/libs/stats.module.js";
-// @ts-ignore
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-// @ts-ignore
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-// @ts-ignore
-import { KTX2Loader } from "three/addons/loaders/KTX2Loader.js";
-// @ts-ignore
-import { MeshoptDecoder } from "three/addons/libs/meshopt_decoder.module.js";
 // @ts-ignore
 import { RoomEnvironment } from "three/addons/environments/RoomEnvironment.js";
 // @ts-ignore
 import { GUI } from "three/addons/libs/lil-gui.module.min.js";
+// @ts-ignore
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+// @ts-ignore
+import Stats from "three/addons/libs/stats.module.js";
+
 import { PAGE_PADDING } from "./constants";
 
 export let mixer: any;
@@ -20,23 +17,15 @@ export let mixer: any;
 export function initThree() {
   const clock = new THREE.Clock();
 
-  const zeroTrack = new THREE.NumberKeyframeTrack(
-    "mesh_2.morphTargetInfluences",
-    [0, 1],
-    Array.from({ length: 104 }, () => 0)
-  );
-  let zeroClip = new THREE.AnimationClip("blink", -1, [zeroTrack]);
-
   const container = document.getElementById("skecth-container");
-  // document.body.appendChild(container);
 
   const camera = new THREE.PerspectiveCamera(
-    45,
+    5,
     window.innerWidth / window.innerHeight,
     1,
     20
   );
-  camera.position.set(-1, 1, 2.5);
+  camera.position.set(-5, 0, 10);
 
   const scene = new THREE.Scene();
 
@@ -51,34 +40,31 @@ export function initThree() {
 
   container?.appendChild(renderer.domElement);
 
-  const light = new THREE.PointLight(0xff0000, 5, 10);
-  light.position.set(0, -3, 4);
+  const light = new THREE.PointLight(0xff0000, 3, 5);
+  light.position.set(-1, 1, 2);
   scene.add(light);
 
-  const ktx2Loader = new KTX2Loader()
-    .setTranscoderPath("/textures/basis/")
-    .detectSupport(renderer);
+  const light2 = new THREE.AmbientLight(0xffffff, 1); // soft white light
+  scene.add(light2);
 
-  new GLTFLoader()
-    .setKTX2Loader(ktx2Loader)
-    .setMeshoptDecoder(MeshoptDecoder)
-    .load("/facecap.glb", (gltf: any) => {
-      console.log("gltf", gltf);
+  const fbxLoader = new FBXLoader();
+  fbxLoader.load(
+    "wiz_nolight.fbx",
+    (object) => {
+      object.scale.set(0.01, 0.01, 0.01);
 
-      const mesh = gltf.scene.children[0];
-      scene.add(mesh);
-      console.log("mesh", mesh);
+      console.log(object);
 
-      mixer = new THREE.AnimationMixer(mesh);
-      mixer.clipAction(zeroClip).play();
-      // GUI
-
-      const head = mesh.getObjectByName("mesh_2");
-      const influences = head.morphTargetInfluences;
+      const mesh = object.children.find(({ type }) => type === "SkinnedMesh");
+      const head = mesh?.getObjectByName("neutral") as THREE.SkinnedMesh;
+      // @ts-ignore
+      const influences = mesh?.morphTargetInfluences;
 
       const gui = new GUI();
       gui.close();
 
+      mixer = new THREE.AnimationMixer(object);
+      // @ts-ignore
       for (const [key, value] of Object.entries(head.morphTargetDictionary)) {
         console.log(key, value);
         gui
@@ -86,7 +72,16 @@ export function initThree() {
           .name(key.replace("blendShape1.", ""))
           .listen(influences);
       }
-    });
+
+      scene.add(object);
+    },
+    (xhr) => {
+      console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
 
   const environment = new RoomEnvironment();
   const pmremGenerator = new THREE.PMREMGenerator(renderer);
@@ -96,15 +91,15 @@ export function initThree() {
 
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.enableDamping = true;
-  controls.minDistance = 2.5;
+  controls.minDistance = 0.5;
   controls.maxDistance = 5;
   controls.minAzimuthAngle = -Math.PI / 2;
   controls.maxAzimuthAngle = Math.PI / 2;
   controls.maxPolarAngle = Math.PI / 1.8;
-  // controls.target.set(0, 0.15, -0.2);
+  // controls.target.set(0, 1, 0);
 
-  // const stats = new Stats();
-  // container.appendChild(stats.dom);
+  const stats = new Stats();
+  container?.appendChild(stats.dom);
 
   renderer.setAnimationLoop(() => {
     const delta = clock.getDelta();
@@ -116,6 +111,6 @@ export function initThree() {
 
     controls.update();
 
-    // stats.update();
+    stats.update();
   });
 }
